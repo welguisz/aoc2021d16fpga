2bb12d169c8bfa6dd705fea8cfb6f4c5eeb833d5
dwelguisz <dwelguisz@indeed.com>
2021-12-17 21:12:38 -0600

    Updated README.md.  Created initial directory structure. Added first pass of the operator module.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # aoc2021d16fpga
 Advent of Code 2021, Day 16 done in work to be synthesizable and placed in a FPGA.
+
+## Goal of this repository
+To create an FPGA representation of the Buoyancy Interchange Transmission System (BITS) as
+described in [Advent of Code, Year 2021, Day 16](https://adventofcode.com/2021/day/16).
+
+## Directory Structure
+* verilog - The actual verilog code that will be synthesizable
+* testbench - Testbench of external devices to the verilog code that will be needed in the real world to make it
+* design_docs - The design docs for each module
+
+## Timeline Goal
+Have a working prototype of simulated in working with the BITS code for my individual problem by 
+Three Kings Day 2022 (January 6, 2022).
+
+## Tools
+* Open Source Verilog Simulator: [Verilator](https://www.veripool.org/verilator/)
---
+++ verilog/operator.v
@@ -0,0 +1,47 @@
+module Operator(
+   //outputs
+   value
+
+   //inputs
+   clk,
+   resetB,
+   input1,
+   input2,
+   type
+   );
+
+   output[63:0] value;
+
+   input clk;
+   input resetB;
+   input [63:0] input1;
+   input [63:0] input2;
+   input [2:0] type;
+
+   reg[63:0] value;
+   reg[63:0] value_next;
+
+   always @(posedge clk or negedge resetB)
+   begin
+      if(~resetB) begin
+          value <= 64'b0;
+      end
+      else begin
+         value <= value_next;
+      end
+   end
+
+   always @(type or input1 or input2)
+   begin
+      value_next = 64'b0;
+      case (type)
+         3'b000 : value_next = input1 + input2;
+         3'b001 : value_next = input1 * input2;
+         3'b010 : value_next = (input1 > input2) ? input1 : input2;
+         3'b011 : value_next = (input1 < input2) ? input1 : input2;
+         3'b101 : value_next = {63'b0, input1 > input2};
+         3'b110 : value_next = {63'b0, input1 < input2}
+         3'b111 : value_next = {63'b0, input1 == input2};
+   end
+
+endmodule
